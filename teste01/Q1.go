9322635f12e82fddd186b51c3718d8dd48096d0f
Davi <136588027+sfDavi@users.noreply.github.com>
2025-03-28 20:55:04 -0300

    Create Q1.go

---
+++ teste01/Q1.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main() {
+  var num float64
+  fmt.Scan(&num)
+  
+  if(num >0){
+      fmt.Println("NUMERO POSITIVO")
+  }else if(num < 0){
+      fmt.Println("NUMERO NEGATIVO")
+  }else{
+      fmt.Println("NUMERO NULO")
+  }
+}
